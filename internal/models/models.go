b3d09976293a2cbc82f601f802aceb693857c446
lipkerton <petr.petuxof@yandex.ru>
2026-06-03 23:30:46 +0300

    models

--- internal/models/models.go
+++ internal/models/models.go
@@ -1,7 +1,21 @@
 package models
 
+type Severity string
+
 const (
-	Low string = "LOW"
-	Medium string = "MEDIUM"
-	High string = "HIGH"
+	Low Severity = "LOW"
+	Medium Severity = "MEDIUM"
+	High Severity = "HIGH"
 )
+
+type Issue struct {
+	Severity Severity `json:"severity" grpc:"severity"`
+	RuleName string `json:"rule_name" grpc:"rule_name"`
+	Description string `json:"description" grpc:"description"`
+	Recommendation string `json:"recommendation" grpc:"recommendation"`
+}
+
+type Rule interface {
+	Name() string
+	Analyze(config map[string]interface{}) []Issue
+}
