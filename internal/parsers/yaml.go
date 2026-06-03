34a31f0853be71c4b2c40287517096226475bc74
lipkerton <petr.petuxof@yandex.ru>
2026-06-03 23:51:17 +0300

    yaml parser

---
+++ internal/parsers/yaml.go
@@ -0,0 +1,18 @@
+package parsers
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+)
+
+type YAMLParser struct{}
+
+func (p *YAMLParser) Parse(data []byte) (map[string]interface{}, error) {
+	var result map[string]interface{}
+
+	if err := yaml.Unmarshal(data, &result); err != nil {
+		return nil, fmt.Errorf("Ошибка парсинга YAML: %w", err)
+	}
+
+	return result, nil
+}
