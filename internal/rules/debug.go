4a1900620d2382249bcbd7316c17177a73d6cff9
lipkerton <petr.petuxof@yandex.ru>
2026-06-04 21:57:55 +0300

    rule debug

---
+++ internal/rules/debug.go
@@ -0,0 +1,36 @@
+package rules
+import (
+	"config_scanner/internal/models"
+)
+
+type DebugRule struct{}
+
+func (r *DebugRule) Name() string { return "Debug Mode Enabled" }
+
+func (r *DebugRule) Analyze(config map[string]interface{}) []models.Issue {
+	var issue []models.Issue
+
+	if val, ok := config["debug"]; ok {
+		if b, ok := val.(bool); ok && b {
+			issues = append(issues, models.Issue{
+				Severity: models.Low,
+				RuleName: r.Name(),
+				Description: "Открытый debug mode.",
+				Recommendation: "Поменяйте режим на более избирательный (info+)."
+			})
+		}
+	}
+
+	if logSub, ok := config["log"].(map[string]interface{}); ok {
+		if level, ok := logSub["level"].(string); ok && levels == "debug" {
+				issues = append(issues, models.Issue{
+				Severity: models.Low,
+				RuleName: r.Name(),
+				Description: "Логирование в debug-режиме.",
+				Recommendation: "Поменяйте режим на более избирательный (info+)."
+			})
+		}
+	}
+
+	return issues
+}
