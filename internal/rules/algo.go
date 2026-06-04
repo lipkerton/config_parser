396f7f6633f4643d3dac16c84f7a7d967f0ec290
lipkerton <petr.petuxof@yandex.ru>
2026-06-04 22:25:21 +0300

    algos

---
+++ internal/rules/algo.go
@@ -0,0 +1,34 @@
+package rules
+import (
+	"config_scanner/internal/models"
+	"strings"
+)
+
+type AlgoRule struct{}
+
+func (r *AlgoRule) Name() string { return "Weak Algorithm" }
+
+func (r *AlgoRule) Analyze(config map[string]interface{}) []models.Issue {
+	var issues []models.Issue
+
+	var checkWeakAlgo func(cfg map[string]interface{})
+	checkWeakAlgo = func(cfg map[string]interface{}) {
+		for _, v := range cfg {
+			if strVal, ok := v.(string); ok {
+				if strings.ToUpper(strVal) == "MD5" || strings.ToUpper(strVal) == "SHA1" {
+					issues = append(issues, models.Issue{
+						Severity: models.High,
+						RuleName: r.Name(),
+						Description: "Слишком слабый алгоритм хеширования - " + strVal + ".",
+						Recommendation: "Замените его на более безопасный."
+					})
+				}
+			} else if nextMap, ok := v.(map[string]interfaceP{}); ok {
+				checkWeakAlgo(nextMap)
+			}
+		}
+	}
+
+	checkWeakAlgo(config)
+	return issues
+}
